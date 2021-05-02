706548d474d1e5c8a36c18e349c4ecc7100718b1
zuzanatrembulakova <zuzana.trembulakova@gmail.com>
2021-05-02 16:35:38 +0200

    home illustrations added

--- script/home-script.js
+++ script/home-script.js
@@ -13,4 +13,37 @@ if (page_type != null && page_type == "nologo") {
     }, 5000);
 }
 
+var mountainCurrentX = 0;
+var beachCurrentX = 0;
+var seaCurrentX = 0;
+var mountin = document.querySelector('.mountains2');
+var beach = document.querySelector('.beach');
+var sea = document.querySelector('.wave2');
 
+window.addEventListener('wheel', function (e) {
+
+    if (mountainCurrentX <= 0) {
+        mountainCurrentX -= e.deltaY / 20;
+        mountin.style.transform = "translateY(" + mountainCurrentX + "px)";
+        if (mountainCurrentX > 0){
+            mountainCurrentX = 0;
+        }
+    }
+
+    if (beachCurrentX <= 0) {
+        beachCurrentX -= e.deltaY / 30;
+        beach.style.transform = "translateY(" + beachCurrentX + "px)";
+        if (beachCurrentX > 0){
+            beachCurrentX = 0;
+        }
+    }
+
+    if (seaCurrentX <= 0) {
+        seaCurrentX -= e.deltaY / 40;
+        sea.style.transform = "translateY(" + seaCurrentX + "px)";
+        if (seaCurrentX > 0){
+            seaCurrentX = 0;
+        }
+    }
+
+});
