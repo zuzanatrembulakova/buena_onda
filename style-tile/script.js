b8a87f84459856d6d64b3066b9270c38ff2d53a8
zuzanatrembulakova <zuzana.trembulakova@gmail.com>
2021-05-10 10:20:53 +0200

    style tile initial commit

---
+++ style-tile/script.js
@@ -0,0 +1,13 @@
+var navIcon = document.querySelector(".nav_icon");
+navIcon.addEventListener("mouseover", mOver, false);
+navIcon.addEventListener("mouseout", mOut, false);
+
+function mOver() {
+    navIcon.classList.remove("icon_out");
+    navIcon.classList.add("icon_in");
+}
+
+function mOut() {
+    navIcon.classList.remove("icon_in");
+    navIcon.classList.add("icon_out");
+}
