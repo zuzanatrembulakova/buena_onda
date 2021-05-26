618cd3565c64e489c89cf5c14e986b681eadf2ab
sara-bertova <57212661+sara-bertova@users.noreply.github.com>
2021-05-26 21:34:20 +0200

    gallery frame experiment

--- script/gallery-script.js
+++ script/gallery-script.js
@@ -14,13 +14,42 @@ function handleGalleryData(jsonData) {
 }
 
 
-// Function for creatin gallery of photoshoots using data retrieved from databes
+// Function for creating gallery using data retrieved from the database
 function showGallery(oneImg) {
     const template = document.querySelector("#gallery_templ").content;
     const clone = template.cloneNode(true);
-
+    const category = oneImg.categories.length;
     clone.querySelector("img").src = oneImg.image.guid;
+    console.log(category);
+    if (category=="2"){
+        clone.querySelector("img").classList.add("frame");
+    }
 
     document.querySelector("main").appendChild(clone);
 }
+
+
+/*fetch("https://rasbery.eu/buena--onda/wp-json/wp/v2/categories")
+    .then(function (response) {
+        return response.json()
+    })
+    .then(function (data) {
+        handleFrameData(data)
+    })
+
+
+function handleFrameData(jsonData) {
+    jsonData.reverse();
+    jsonData.forEach(showFrames);
+}
+
+function showFrames(oneFrame) {
+    const category = oneFrame.name;
+    console.log(category)
+    if (category=="frame"){
+        document.querySelector("img").classList.add = "frame";
+    }
+}*/
+
+
 
